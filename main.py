a86ba6766e4f0e47629bb8c8b92cb5eccc4f43a8
brahian.alvarez <brahian.alvarez@quipux.com>
2024-08-26 20:27:03 -0500

    feat: merge cambios y agrega black al archivo

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from datetime import datetime
 
 app = FastAPI()
 
+
 @app.get("/lista-ordenada", tags=["lista-ordenada"])
 def ordenar_lista(lista_no_ordenada: List[int]):
     lista_ordenada = sorted(lista_no_ordenada)
     hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return {
-        "hora_sistema": hora_actual,
-        "lista_ordenada": lista_ordenada
-    }
+    return {"hora_sistema": hora_actual, "lista_ordenada": lista_ordenada}
+
 
 @app.get("/healthcheck")
 def healthcheck():

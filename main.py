736beadc4e2a973333bd4d480b4e1d2e5349f200
brahian.alvarez <brahian.alvarez@quipux.com>
2024-08-26 19:53:13 -0500

    feat: agrega endpoint ordenar_lista

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from typing import List
+from datetime import datetime
+
+app = FastAPI()
+
+@app.get("/lista-ordenada", tags=["lista-ordenada"])
+def ordenar_lista(lista_no_ordenada: List[int]):
+    lista_ordenada = sorted(lista_no_ordenada)
+    hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return {
+        "hora_sistema": hora_actual,
+        "lista_ordenada": lista_ordenada
+    }

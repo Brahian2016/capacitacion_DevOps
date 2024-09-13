dd724fa4ec07a4a47adfc7a0e8e4105a77c2b361
Brahian2016 <52221201+Brahian2016@users.noreply.github.com>
2024-09-13 02:46:42 +0000

    feat: agrega endpoint para guardar datos a mongo

--- api/main.py
+++ api/main.py
@@ -1,45 +1,71 @@
-from fastapi import FastAPI, Query
-from typing import List
-from datetime import datetime
-from fastapi.responses import JSONResponse
-from pymongo import MongoClient
+"""
+Este módulo define una API con FastAPI que incluye los siguientes endpoints:
+- /lista-ordenada: para ordenar una lista de números.
+- /guardar-lista-no-ordenada: para guardar una lista no ordenada en la base de datos.
+- /healthcheck: para verificar el estado de la API.
+"""
+
+# Importaciones de librerías estándar
 import os
 import uuid
+from datetime import datetime
+from typing import List
 
-# Inicialización de la aplicación FastAPI
+# Importaciones de librerías externas
+from fastapi import FastAPI, Query, HTTPException
+from fastapi.responses import JSONResponse
+
+# Importaciones de módulos internos
+from db.mongodb import connect_to_mongo, collection
+from models.document import Document
+
 app = FastAPI()
 
-# Conexión a MongoDB utilizando variables de entorno para el host y el puerto.
-MONGODB_HOST = os.getenv("MONGODB_HOST", "localhost")
-MONGODB_PORT = os.getenv("MONGODB_PORT", "27017")
+# Conectar a MongoDB
+connect_to_mongo()
 
-cliente = MongoClient(f'mongodb://{MONGODB_HOST}:{MONGODB_PORT}/')
-db = cliente.python_app
-collection = db.listas_no_ordenadas
 
 @app.get("/lista-ordenada", response_class=JSONResponse)
 def ordenar_lista(
     lista_no_ordenada: List[int] = Query(
-        ..., description="Lista de números no ordenados que se desea ordenar"
-        , alias="lista-no-ordenada"
+        ...,
+        description="Lista de números no ordenados que se desea ordenar",
+        alias="lista-no-ordenada",
     )
 ) -> JSONResponse:
     """
     Endpoint para ordenar una lista de números.
-
-    Args:
-        lista_no_ordenada (List[int]): Lista de números enteros que se deben ordenar.
     """
-    # Ordenar la lista recibida
     lista_ordenada = sorted(lista_no_ordenada)
-
-    # Obtener la hora actual en formato "YYYY-MM-DD HH:MM:SS"
     hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Crear la respuesta en formato JSON
     response = {"hora_sistema": hora_actual, "lista_ordenada": lista_ordenada}
 
-    return JSONResponse(content=response, media_type="application/json")
+    return JSONResponse(content=response)
+
+
+@app.get("/guardar-lista-no-ordenada", response_class=JSONResponse)
+def guardar_lista_no_ordenada(
+    lista_no_ordenada: List[int] = Query(..., alias="lista-no-ordenada")
+) -> JSONResponse:
+    """
+    Endpoint para guardar una lista no ordenada en la base de datos.
+    """
+    lista_ordenada = sorted(lista_no_ordenada)
+    hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    unique_id = str(uuid.uuid4())
+
+    document = Document(
+        id=unique_id,
+        hora_sistema=hora_actual,
+        lista_no_ordenada=lista_no_ordenada,
+        lista_ordenada=lista_ordenada,
+    )
+
+    collection.insert_one(document.to_dict())
+
+    response = {"msg": f"La lista ordenada fue guardada con el id: {unique_id}"}
+    return JSONResponse(content=response)
 
 
 @app.get("/healthcheck")
@@ -48,23 +74,3 @@ def healthcheck() -> str:
     Endpoint para verificar el estado de la API.
     """
     return "OK"
-
-@app.get("/guardar-lista-no-ordenada", response_class=JSONResponse)
-def guardar_lista_no_ordenada(
-    lista_no_ordenada: List[int] = Query(..., alias="lista-no-ordenada")
-):
-    lista_ordenada = sorted(lista_no_ordenada)
-    hora_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    unique_id = str(uuid.uuid4())
-
-    # Guardar la información en MongoDB
-    document = {
-        "id": unique_id,
-        "hora_sistema": hora_actual,
-        "lista_no_ordenada": lista_no_ordenada,
-        "lista_ordenada": lista_ordenada
-    }
-    collection.insert_one(document)
-
-    response = {"msg": f"La lista ordenada fue guardada con el id: {unique_id}"}
-    return JSONResponse(content=response)
